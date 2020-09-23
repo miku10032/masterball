57b8d8c3aed030aeeac25a65b72a666c3cff7063
jubao <tqy_4896@hotmail.com>
2020-09-23 10:43:14 +0800

    Initial Commit

---
+++ masterball/masterballApp.swift
@@ -0,0 +1,17 @@
+//
+//  masterballApp.swift
+//  masterball
+//
+//  Created by User16 on 2020/9/23.
+//
+
+import SwiftUI
+
+@main
+struct masterballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
